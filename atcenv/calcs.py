b929a1edeeb757b75b9d43469822e72ddb18d1db
vera15380 <weronika.prawda@estudiantat.upc.edu>
2022-06-04 15:26:36 +0200

    adding angle safe turn func

--- atcenv/calcs.py
+++ atcenv/calcs.py
@@ -39,6 +39,71 @@ def d_cpa(env, i: int, j: int) -> float:
     return dcpa
 
 
+def safe_turn_angle(env, i: int, j: int) -> float:
+    """
+    def function(angle):
+        dx = env.flights[j].position.x - env.flights[i].position.x
+        dy = env.flights[j].position.y - env.flights[i].position.y
+        tcpa = max(0, -(dx * vx[1] + dy * vx[2]) / (vx[1] ** 2 + vx[2] ** 2))
+        return [math.sqrt((dx + vx[1] * tcpa) ** 2 + (dy + vx[2] * tcpa) ** 2) - dcpa, math.atan2(vx[1], vx[2]) - angle]
+    """
+    tol = 100
+    angle_right = 0
+    while angle_right < (180 * (u.circle / 360)):
+        track = env.flights[i].track + angle_right
+        dx = env.flights[j].position.x - env.flights[i].position.x
+        dy = env.flights[j].position.y - env.flights[i].position.y
+        vrx = env.flights[j].components[0] - env.flights[i].airspeed * math.sin(track)
+        vry = env.flights[j].components[1] - env.flights[i].airspeed * math.cos(track)
+        tcpa = max(0, -(dx * vrx + dy * vry) / (vrx ** 2 + vry ** 2))
+        dcpa = math.sqrt((dx + vrx * tcpa) ** 2 + (dy + vry * tcpa) ** 2)
+
+        tol = env.alert_distance - dcpa
+        if tol < 0 or tcpa == 0:
+            break
+        angle_right = angle_right + (1 * (u.circle / 360))
+
+    tol = 100
+    angle_left = 0
+    while angle_left > (-180 * (u.circle / 360)):
+        track = env.flights[i].track + angle_left
+        dx = env.flights[j].position.x - env.flights[i].position.x
+        dy = env.flights[j].position.y - env.flights[i].position.y
+        vrx = env.flights[j].components[0] - env.flights[i].airspeed * math.sin(track)
+        vry = env.flights[j].components[1] - env.flights[i].airspeed * math.cos(track)
+        tcpa = max(0, -(dx * vrx + dy * vry) / (vrx ** 2 + vry ** 2))
+        dcpa = math.sqrt((dx + vrx * tcpa) ** 2 + (dy + vry * tcpa) ** 2)
+
+        tol = env.alert_distance - dcpa
+        if tol < 0 or tcpa == 0:
+            break
+        angle_left = angle_left - (1 * (u.circle / 360))
+
+    """ Choosing whether turn right or turn left """
+    if env.flights[i].position.distance(env.flights[j].position) < env.alert_distance:   # Same as tcpa == 0
+        # Computing track right and verifying that the angle is between 0 and 2*phi
+        track_right = env.flights[i].track + angle_right
+        if track_right > 2*u.circle:
+            track_right = track_right - u.circle
+        # Computing track left and verifying that the angle is between 0 and 2*phi
+        track_left = env.flights[i].track + angle_left
+        if track_left < 0:
+            track_left = track_left + u.circle
+        # Computing...
+        dif_right = abs(track_right - env.flights[j].track)
+        dif_left = abs(track_left - env.flights[j].track)
+        if dif_right > dif_left:
+            angle_safe_turn = angle_right
+        else:
+            angle_safe_turn = angle_left
+    else:
+        angle_safe_turn = min(abs(angle_right), abs(angle_left))
+        if angle_safe_turn == abs(angle_left):
+            angle_safe_turn = angle_left
+
+    return angle_safe_turn
+
+
 def sector_assignment(rel_angle):
     converge = (110 / 360) * u.circle  # 110º
     head_on = (15 / 360) * u.circle    # 15º
